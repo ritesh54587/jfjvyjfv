06758b0a6725eda7a54af9de5c5170234ae953fd
ritesh54587 <riteshkumaryadav.sangamone@gmail.com>
2021-11-30 10:15:33 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      employees:null
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h2>Employees Data...</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>FirstName</th>
+              <th>LastName</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>         
+          </tbody>
+        </table>
+      </div>
+      );
+    }
+
+    {this.state.employees.map(emp => (
+      <tr key={emp.id}>
+        <td>{emp.first_name}</td>
+        <td>{emp.last_name}</td>
+        <td>{emp.email}</td>
+        
+        </tr>
+))}
+  componentDidMount()
+  {
+    Fetch("https://reqres.in/api/users")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            employees: result
+          });
+        }
+      );
 }
 
 export default App;
